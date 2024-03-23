191a47416566156a666c932002d2923622f3993d
VenusBridget <venusbri27@gmail.com>
2024-03-23 05:04:48 +0300

    first commit

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
-
-// Write your code here
+let num1 = 2;
+let num2 = 31;
+let multiply = (num1) * (num2);
+console.log(multiply);
+// console.log--------------------
+let random = Math.floor(Math.random() * 7);
+console.log(random);
+// console.log--------------------
+let num3 = 18;
+let num4 = 7;
+let mod = (num3) % (num4);
+console.log(mod);
+// console.log---------------------
+let max = Math.max(5,10,15,20);
+console.log(max);
